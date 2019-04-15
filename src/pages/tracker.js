3fe8b4d48e6745a9b27af0c5e4f84e26ce716a75
Victor Diaz de Leon <vrdiazdel@gmail.com>
2019-04-14 22:22:42 -0700

    Create tracker page

---
+++ src/pages/tracker.js
@@ -0,0 +1,16 @@
+import React from "react"
+
+import Layout from "../components/layout"
+import SEO from "../components/seo"
+import Wrapper from "../elements/wrapper"
+
+const Tracker = () => (
+  <Layout>
+    <SEO title="Kombucha Tracker" />
+    <Wrapper className="mt-5">
+      <h1>Kombucha Tracker</h1>
+    </Wrapper>
+  </Layout>
+)
+
+export default Tracker
